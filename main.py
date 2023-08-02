e193e865ca0b634267aa080790f11419c95946d6
JLMA <103587815+JLMA-Pro-Trading@users.noreply.github.com>
2023-08-02 17:43:50 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template, request
+
+import base64
+from io import BytesIO
+
+from diffusers import StableDiffusionXLPipeline
+import torch
+
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+)
+pipe.to("cuda")
+
+# Start flask app and set to ngrok
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def initial():
+  return render_template('index.html')
+
+
+@app.route('/submit-prompt', methods=['POST'])
+def generate_image():
+  prompt = request.form['prompt-input']
+  print(f"Generating an image of {prompt}")
+
+  image = pipe(prompt).images[0]
+  print("Image generated! Converting image ...")
+  
+  buffered = BytesIO()
+  image.save(buffered, format="PNG")
+  img_str = base64.b64encode(buffered.getvalue())
+  img_str = "data:image/png;base64," + str(img_str)[2:-1]
+
+  print("Sending image ...")
+  return render_template('index.html', generated_image=img_str)
+
+
+if __name__ == '__main__':
+    app.run()
